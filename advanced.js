ee24ebb212c43f8025cf63e8009db3e3d2032711
markus kamitz <markus_kamitz@yahoo.de>
2021-07-08 14:54:19 +0200

    js files with serri's fix

--- advanced.js
+++ advanced.js
@@ -1,23 +1,41 @@
-var Vehicle = /** @class */ (function () {
-    function Vehicle(image, brand, model, pYear, kilometers) {
+"use strict";
+class Vehicle {
+    constructor(image, brand, model, pYear, kilometers) {
         this.image = image;
         this.brand = brand;
         this.model = model;
         this.pYear = pYear;
         this.kilometers = kilometers;
     }
-    Vehicle.prototype.printCard = function () {
-        return "<div class=\"card col-4\" style=\"width: 18rem;\">\n      <img src=\"" + this.image + "\" class=\"card-img-top\" >\n      <div class=\"card-body\">\n        <h5 class=\"card-title\">" + this.brand + "</h5>\n        <p class=\"card-text\">Model : " + this.model + " <br> Year: " + this.pYear + " <br></p>\n        <a href=\"#\" class=\"btn btn-primary\" id=\"btn\">Show more</a>\n      </div>\n    </div>\n     ";
-    };
-    Vehicle.prototype.calculatePrice = function () {
-        var price = this.kilometers + this.pYear;
-        return "" + price;
-    };
-    Vehicle.prototype.details = function () {
-        document.getElementById("container").innerHTML = "\n        <div class=\"card col-4\" style=\"width: 18rem;\">\n        <img src=\"" + newVehicle.image + "\" class=\"card-img-top\" >\n        <div class=\"card-body\">\n          <h5 class=\"card-title\">" + newVehicle.brand + "</h5>\n          <p class=\"card-text\">Model : " + newVehicle.model + " <br> Year: " + newVehicle.pYear + " <br></p>\n          <p class=\"card-text\">Kilometers : " + newVehicle.kilometers + " <br> Price: " + newVehicle.calculatePrice() + " <br></p>\n          \n        </div>\n      </div>\n     ";
-    };
-    return Vehicle;
-}());
+    printCard() {
+        return `<div class="card col-4" style="width: 18rem;">
+      <img src="${this.image}" class="card-img-top" >
+      <div class="card-body">
+        <h5 class="card-title">${this.brand}</h5>
+        <p class="card-text">Model : ${this.model} <br> Year: ${this.pYear} <br></p>
+        <a href="#" class="btn btn-primary" id="btn">Show more</a>
+      </div>
+    </div>
+     `;
+    }
+    calculatePrice() {
+        let price = this.kilometers + this.pYear;
+        return `${price}`;
+    }
+    details() {
+        document.getElementById("container").innerHTML = `
+        <div class="card col-4" style="width: 18rem;">
+        <img src="${newVehicle.image}" class="card-img-top" >
+        <div class="card-body">
+          <h5 class="card-title">${newVehicle.brand}</h5>
+          <p class="card-text">Model : ${newVehicle.model} <br> Year: ${newVehicle.pYear} <br></p>
+          <p class="card-text">Kilometers : ${newVehicle.kilometers} <br> Price: ${newVehicle.calculatePrice()} <br></p>
+          
+        </div>
+      </div>
+     `;
+    }
+}
 var newVehicle = new Vehicle("https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930__340.jpg", "Ford", "Mustang", 1968, 10000);
 document.getElementById("container").innerHTML = newVehicle.printCard();
 document.getElementById("btn").addEventListener("click", newVehicle.details);
